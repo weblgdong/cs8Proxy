451cd6482cd265e995992f749779ecb69f3b56e5
weblgdong <weblgdong@gmail.com>
2018-05-16 10:11:05 +0800

    update

--- a.js
+++ a.js
@@ -18,18 +18,22 @@ app.listen(4001)
 async function imgtoBase64(url) {
   var url = url // || 'http://www.csby8.com:8000/images/csw/32A5AF7AB3E44824A902092873B5893F.jpg';
   return new Promise((resolve, reject) => {
-    http.get(url, function (res) {　　
-      var chunks = [];
-      var size = 0;
-      res.on('data', function (chunk) {　　　　
-        chunks.push(chunk);　
-        size += chunk.length;
+    if (!url) {
+      reject('');
+    } else {
+      http.get(url, function (res) {　　
+        var chunks = [];
+        var size = 0;
+        res.on('data', function (chunk) {　　　　
+          chunks.push(chunk);　
+          size += chunk.length;
+        });
+        res.on('end', function (err) {
+          var data = Buffer.concat(chunks, size);　
+          var base64Img = data.toString('base64');
+          resolve('data:image/jpeg;base64,' + base64Img);
+        });
       });
-      res.on('end', function (err) {
-        var data = Buffer.concat(chunks, size);　
-        var base64Img = data.toString('base64');
-        resolve('data:image/jpeg;base64,' + base64Img);
-      });
-    });
+    }
   })
 }
